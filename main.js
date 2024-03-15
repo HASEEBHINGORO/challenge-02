a51bc3ec3c5444ff657e0cc525947894e5feca67
ABDUL HASEEB MUHAMMAD HAROON <163283484+HASEEBHINGORO@users.noreply.github.com>
2024-03-15 15:20:17 +0500

    Add files via upload
    Challenge 02 Complete All Question

---
+++ main.js
@@ -0,0 +1,9 @@
+// let famousperson:string =  "QUAID-E-AZAM once said,Aperson who never made a mistake never tried anything new."
+// console.log(famousperson);
+// let famous_person :string = "QUAID-E-AZAMA once said, A person who never made a mistake never tried anything new"
+// let message = (famous_person)
+// console.log(message)
+var name = "\t HASEEB HAROON\N";
+console.log('Name with whitespace: "${HASEEB HAROON}"');
+var strippedname = name.trim();
+console.log('Strippedname :"${HASEEB HAROON}"');
---
+++ main.ts
@@ -0,0 +1,12 @@
+let famousperson:string =  "QUAID-E-AZAM once said,Aperson who never made a mistake never tried anything new."
+console.log(famousperson);
+
+
+let famous_person :string = "QUAID-E-AZAMA once said, A person who never made a mistake never tried anything new"
+let message = (famous_person)
+console.log(message)
+
+let name : string = "\t HASEEB HAROON\N";
+console.log('Name with whitespace: "${HASEEB HAROON}"');
+let strippedname : string = name.trim();
+console.log('Strippedname :"${HASEEB HAROON}"');
